c0fd05ce9c8f7b3141b5ca7159f365270fea5e3e
Broccolito <wanjungu001@gmail.com>
2024-02-21 16:41:01 -0800

    automatic commit

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+utils::globalVariables(c("fid1", "fid1_iid1", 
+                         "fid2", "fid2_iid2", 
+                         "iid1", "iid2", "related",
+                         "wt1", "wt2"))
